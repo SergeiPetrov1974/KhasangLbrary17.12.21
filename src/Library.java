ca72bd78ea8d6b64b3e9d78b4a706915bf442af4
Сергей <040620@mail.ru>
2021-12-13 14:14:37 +0300

    Task to check

---
+++ src/Library.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private static int counter;
+    private List<Book> catalog = new ArrayList<>();
+    private int counterId;
+
+    public Library() {
+        counter++;
+        counterId = counter;
+    }
+
+    public List<Book> getCatalog() {
+        return catalog;
+    }
+
+    public void addBook(Book book) {
+        catalog.add(book);
+        System.out.println("Книга №" + getCountBook() + " " + book.getBookInfo());
+    }
+
+    public void removeBook(int id) {
+        catalog.remove(id);
+    }
+
+    public int getCountBook() {
+        return catalog.size();
+
+    }
+
+    public String getBookInfoById(int id) {
+        for (int i = 0; i < catalog.size(); i++) {
+            return catalog.get(id).getBookInfo();
+        }
+        return null;
+    }
+
+    public int searchBookByName(String text) {
+        String nameBook;
+        for (int i = 0; i < getCountBook(); i++) {
+            nameBook = catalog.get(i).getName().toLowerCase();
+            if (nameBook.contains(text.toLowerCase())) {
+                System.out.println(catalog.get(i).getBookInfo());
+                counter++;
+            }
+        }
+        if (counter == 0) {
+            counter = searchBookByWriter(text);
+        }
+        return counter;
+    }
+
+    public int searchBookByWriter(String text) {
+        String nameWriter;
+        for (int i = 0; i < getCountBook(); i++) {
+            nameWriter = catalog.get(i).getAuthor().toLowerCase();
+            if (nameWriter.contains(text.toLowerCase())) {
+                System.out.println(catalog.get(i).getBookInfo());
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/Menu.java
@@ -0,0 +1,95 @@
+public class Menu {
+    static Library library = new Library();
+    static Reader reader = new Reader();
+
+
+    public static void start() {
+        int chooseAction = 0;
+        System.out.println("Сейчас в каталоге книг - " + library.getCountBook());
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Что будем делать: 0-Добавить книгу; 1-Найти книгу; 2-Удалить книгу");
+            chooseAction = selectAction(reader);
+            switch (chooseAction) {
+                case 0:
+                    addLibraryBook(reader, library);
+                    System.out.println("В каталоге книг - " + library.getCountBook());
+                    break;
+                case 1:
+                    if (library.getCountBook() == 0) {
+                        System.out.println("Каталог пустой.");
+                    } else {
+                        searchBooks();
+                    }
+                    break;
+
+                case 2:
+                    if (library.getCountBook() > 0) {
+                        removeLibraryBook(reader, library);
+                        System.out.println("В каталоге осталось книг - " + library.getCountBook());
+                    } else {
+                        System.out.println("В каталоге пусто. Нет книг для удаления");
+                        break;
+                    }
+                default:
+                    return;
+            }
+        }
+    }
+
+    static int selectAction(Reader reader) {
+        int action;
+        do {
+            action = reader.getAnswer();
+            if (action != 0 & action != 1 & action != 2) {
+                System.out.println("Укажите что нужно сделать: 0-Добавить книгу; 1-Найти книгу по названию" + " 2-Удалить книгу");
+            }
+        } while (action != 0 & action != 1 & action != 2);
+        return action;
+    }
+
+    public static void searchBooks() {
+        System.out.println("Какую книгу вы ищете сегодня?");
+        String bookSearch = reader.getString();
+        for (Book book : library.getCatalog()) {
+            if (book.getName().contains(bookSearch) || book.getAuthor().contains(bookSearch)) {
+                System.out.println("Книга найдена " + book.getBookInfo());
+            } else {
+                System.out.println("Книга не найдена ");
+            }
+        }
+    }
+
+    static void addLibraryBook(Reader reader, Library library) {
+        String name = null;
+        String author = null;
+        int publicationYear = 0;
+        System.out.println("Укажите название книги");
+        name = reader.getString();
+        System.out.println("Укажите автора книги");
+        author = reader.getString();
+        System.out.println("Укажите год публикации");
+        publicationYear = Integer.parseInt(reader.getString());
+        library.addBook(new Book(name, author, publicationYear));
+        System.out.println("Книга добавлена в каталог");
+    }
+
+    static void removeLibraryBook(Reader reader, Library catalog) {
+        int numBook;
+        System.out.println("В каталоге книг - " + catalog.getCountBook());
+        System.out.println("Укажите номер удаляемой книги");
+        do {
+            numBook = reader.getAnswer();
+        } while (numBook < 1 || numBook > catalog.getCountBook());
+        --numBook;
+        System.out.println("Вы уверены что хотите удалить книгу: " + catalog.getBookInfoById(numBook) + "?");
+        System.out.println("0-нет; 1-да");
+        int ans;
+        do {
+            ans = reader.getAnswer();
+        } while (ans != 0 & ans != 1);
+        if (ans == 1) {
+            catalog.removeBook(numBook);
+        }
+    }
+}
+
